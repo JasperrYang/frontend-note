c534e6c98bf5c6df16ec3443d74a214561ae5ee7
jasper.yang <lupeng3381@163.com>
2021-10-28 15:52:03 +0800

    vuex/nuxt: init project

---
+++ 03-02-Vuex/NuxtJs/realworld/engine/user.js
@@ -0,0 +1,12 @@
+import { request } from '@/utils/request'
+
+
+// 用户登录
+export const login = data => {
+  return request.post('/api/users/login', data)
+}
+
+
+export const register = data => {
+  return request.post('/api/users', data)
+}
